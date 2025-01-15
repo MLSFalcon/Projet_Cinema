e265be78f7dde7bc0682bf00ce8a451c558f1fde
Theo Enfedaque <t.enfedaque@lprs.fr>
2025-01-15 09:01:04 +0100

    fix(bdd) : changement de lien d'accès à la bdd

--- includes/bdd.php
+++ includes/bdd.php
@@ -1,4 +1,4 @@
 <?php
-$bdd = new PDO('mysql:host=82.67.133.174;dbname=mnrt_cinema;charset=utf8', 'bdd_cinema', 'jozdZSM#9');
+$bdd = new PDO('mysql:host=hoziodev.fr;dbname=mnrt_cinema;charset=utf8', 'bdd_cinema', 'jozdZSM#9');
 
 return $bdd;
