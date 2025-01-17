52e36296b2e70a9ff8a06c0d03efa339e1077bc9
Theo Enfedaque <t.enfedaque@lprs.fr>
2025-01-17 08:28:11 +0100

    refactor(bdd): changement en objet

--- includes/bdd.php
+++ includes/bdd.php
@@ -1,4 +1,16 @@
 <?php
-$bdd = new PDO('mysql:host=hoziodev.fr;dbname=mnrt_cinema;charset=utf8', 'bdd_cinema', 'jozdZSM#9');
+class Bdd
+{
 
-return $bdd;
+    private $bdd;
+
+    function __construct()
+    {
+        $this->bdd = new PDO('mysql:host=hoziodev.fr;dbname=mnrt_cinema;charset=utf8', 'bdd_cinema', 'jozdZSM#9');
+    }
+
+    function getBdd()
+    {
+        return $this->bdd;
+    }
+}
