17b9d08a9d88eff05e491a5e0d03c92f2db03d50
Mateo LAPETRE <m.lapetre-sarthou@lprs.fr>
2025-01-22 11:22:42 +0100

    refactor: changement des classe Film/reservation

--- src/class/Reservation.php
+++ src/class/Reservation.php
@@ -3,5 +3,86 @@
 
 class Reservation
 {
+    private $idReservation;
+    private $nbPlace;
+    private $refUser;
+    private $refSeance;
 
+    private function hydrate($array) {
+        foreach ($array as $key => $value) {
+            $method = 'set'.ucfirst($key);
+
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+    public function __construct($array)
+    {
+        $this->hydrate($array);
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function getIdReservation()
+    {
+        return $this->idReservation;
+    }
+
+    /**
+     * @param mixed $idReservation
+     */
+    public function setIdReservation($idReservation)
+    {
+        $this->idReservation = $idReservation;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNbPlace()
+    {
+        return $this->nbPlace;
+    }
+
+    /**
+     * @param mixed $nbPlace
+     */
+    public function setNbPlace($nbPlace)
+    {
+        $this->nbPlace = $nbPlace;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRefUser()
+    {
+        return $this->refUser;
+    }
+
+    /**
+     * @param mixed $refUser
+     */
+    public function setRefUser($refUser)
+    {
+        $this->refUser = $refUser;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRefSeance()
+    {
+        return $this->refSeance;
+    }
+
+    /**
+     * @param mixed $refSeance
+     */
+    public function setRefSeance($refSeance)
+    {
+        $this->refSeance = $refSeance;
+    }
 }
