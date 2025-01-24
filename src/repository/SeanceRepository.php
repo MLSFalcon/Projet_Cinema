d7b1bcef3838a2bd8051805a3c811f9de2f1c552
Nico180803 <nicolascharpentier1808@gmail.com>
2025-01-24 09:23:57 +0100

    feat: repository

--- src/repository/SeanceRepository.php
+++ src/repository/SeanceRepository.php
@@ -1,6 +1,43 @@
 <?php
 
 class SeanceRepository
-{
+{   public function nouvelleSeance($seance){
+    $bddUser = new Bdd();
+
+    $req = $bddUser->getBdd()->prepare('INSERT INTO date_seance(date_seance, heure, ref_salle, ref_film, prix) VALUES(:date_seance, :heure, :ref_salle, :ref_film, :prix)');
+    $req->execute(array(
+        'date_seance' => $seance->getDateSeance(),
+        'heure' => $seance->getHeure(),
+        'ref_salle' => $seance->getRefSalle(),
+        'ref_film' => $seance->getRefFilm(),
+        'prix' => $seance->getPrix()
+    ));
+    $req->closeCursor();
+    return true;
+}
+
+    public function updateSeance($seance)
+    {
+        $bddUser = new Bdd();
+        $reqModif = $bddUser->getBdd()->prepare("UPDATE seance SET date_seance = :date_seance, heure = :heure, ref_salle=:ref_salle, ref_film=:ref_film, prix=:prix WHERE id_seance = :id");
+        $reqModif->execute(array(
+            'date_seance' => $seance->getDateSeance(),
+            'heure' => $seance->getHeure(),
+            'ref_salle' => $seance->getRefSalle(),
+            'ref_film' => $seance->getRefFilm(),
+            'prix' => $seance->getPrix()
+        ));
+        $reqModif->closeCursor();
+    }
+
+    public function suppSeance($seance)
+    {
+        $bddUser = new Bdd();
+        $reqSupp = $bddUser->getBdd()->prepare("DELETE FROM seance WHERE id_seance = :id");
+        $reqSupp->execute(array(
+            'id' => $seance->getIdSeance()
+        ));
+        $reqSupp->closeCursor();
+    }
 
 }
