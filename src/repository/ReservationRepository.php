63d25e9b8f3d0b4d09ad8e723ea1e94ebe407f32
Theo Enfedaque <t.enfedaque@lprs.fr>
2025-01-24 09:29:27 +0100

    feat(ReservationRepo): debut du repo

--- src/repository/ReservationRepository.php
+++ src/repository/ReservationRepository.php
@@ -3,4 +3,16 @@
 class ReservationRepository
 {
 
+    private $bdd;
+
+public function __construct(){
+    $this->bdd = new Bdd();
+}
+
+public function ajouter($reservation)
+{
+    
+    $req = $this->bdd->getBdd()->prepare('INSERT INTO seance(nb_place, ref_user, ref_seance) VALUES(:nb_place, :ref_user, :ref_seance)');
+}
+
 }
