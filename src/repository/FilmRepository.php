506b9dd379bae48bb2b385bcd944c7d7d77dc3ce
Mateo LAPETRE <m.lapetre-sarthou@lprs.fr>
2025-01-24 09:48:57 +0100

    refactor!: changement des page admin/index pour correspondre a FilmRepo

--- src/repository/FilmRepository.php
+++ src/repository/FilmRepository.php
@@ -1,6 +1,61 @@
 <?php
-
 class FilmRepository
 {
+    /**
+     * @param Film $film
+     * @return void
+     */
+    public function modifier($film)
+    {
+        $reqModif = $this->bdd->getBdd()->prepare("UPDATE film SET image = :image, titre = :titre, resume = :resume, genre = :genre, duree = :duree WHERE id_film = :id");
+        $reqModif->execute(array(
+            "titre" => $film->getTitre(),
+            "resume" => $film->getResume(),
+            "genre" => $film->getGenre(),
+            "duree" => $film->getDuree(),
+            "image" => $film->getImage(),
+            "id" => $film->getIdFilm()
+        ));
+        $reqModif->closeCursor();
+    }
 
+    /**
+     * @param Film $film
+     * @return void
+     */
+    public function ajouter($film)
+    {
+        $bdd = new Bdd();
+        $reqModif = $bdd->getBdd()->prepare("INSERT INTO film (titre, resume, genre, duree, image) VALUES (:titre, :resume, :genre, :duree, :image)");
+        $reqModif->execute(array(
+            "titre" => $film->getTitre(),
+            "resume" => $film->getResume(),
+            "genre" => $film->getGenre(),
+            "duree" => $film->getDuree(),
+            "image" => $film->getImage()
+        ));
+        $reqModif->closeCursor();
+    }
+
+    /**
+     * @param Film $film
+     * @return void
+     */
+    public function supprimer($film)
+    {
+        $bdd = new Bdd();
+        $reqSupp = $bdd->getBdd()->prepare("DELETE FROM film WHERE id_film = :id");
+        $reqSupp->execute(array(
+            'id' => $film->getIdFilm()
+        ));
+        $reqSupp->closeCursor();
+    }
+    public function listeFilms(){
+        $bdd = new Bdd();
+        $requete = $bdd->getBdd()->prepare("SELECT * FROM `film`");
+        $requete->execute();
+        $listeFilms = $requete->fetchAll();
+        $requete->closeCursor();
+        return $listeFilms;
+    }
 }
