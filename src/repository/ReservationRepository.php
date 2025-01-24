05ed17a2ee58730623e05478cd97d3ccfeb6558d
Theo Enfedaque <t.enfedaque@lprs.fr>
2025-01-24 09:49:18 +0100

    feat(ReservationRepo): rajout de toutes les méthodes

--- src/repository/ReservationRepository.php
+++ src/repository/ReservationRepository.php
@@ -2,7 +2,6 @@
 
 class ReservationRepository
 {
-
     private $bdd;
 
 public function __construct(){
@@ -11,8 +10,41 @@ public function __construct(){
 
 public function ajouter($reservation)
 {
-    
     $req = $this->bdd->getBdd()->prepare('INSERT INTO seance(nb_place, ref_user, ref_seance) VALUES(:nb_place, :ref_user, :ref_seance)');
+    $req->execute(array(
+        'nb_place' => $reservation->getNbPlace(),
+        'ref_user' => $reservation->getRefUser(),
+        'ref_seance' => $reservation->getRefSeance()
+    ));
+    $req->closeCursor();
+    return true;
+}
+
+public function update($reservation)
+{
+    $req = $this->bdd->getBdd()->prepare('UPDATE seance SET nb_place = :nb_place, ref_user = :ref_user, ref_seance = :ref_seance WHERE id_reservation = :id_reservation');
+    $req->execute(array(
+        'nb_place' => $reservation->getNbPlace(),
+        'ref_user' => $reservation->getRefUser(),
+        'ref_seance' => $reservation->getRefSeance(),
+        'id_reservation' => $reservation->getIdReservation()
+    ));
+    $req->closeCursor();
+    return true;
+}
+
+public function delete($reservation)
+{
+    $req = $this->bdd->getBdd()->prepare('DELETE FROM reservation WHERE id_reservation = :id_reservation');
+    $req->execute(array(
+        'id_reservation' => $reservation->getIdReservation()
+    ));
+    $req->closeCursor();
+    return true;
+}
+
+public function listeReservation($reservation){
+    return $reservation;
 }
 
 }
