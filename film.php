94d8813794629520a548159043498a1269f93ca0
Mateo LAPETRE <m.lapetre-sarthou@lprs.fr>
2025-01-29 10:21:54 +0100

    refactor: correction du bouton retour vers l'accueil

--- film.php
+++ film.php
@@ -44,7 +44,7 @@ session_start();
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="film.php">Films</a></li>
+                <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="index.php">Accueil</a></li>
                 <?php
                 if (isset($_SESSION['user'])) {
                     echo '
