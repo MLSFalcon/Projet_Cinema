393f5d9912cd6ff8cd389252014c547bd1176cfc
Nico180803 <nicolascharpentier1808@gmail.com>
2025-02-06 16:19:05 +0100

    fix: envoie mail fonctionne (pas finit)

--- src/traitement/gestionMail.php
+++ src/traitement/gestionMail.php
@@ -4,26 +4,32 @@
 require '../../vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 
-
 $mail = new PHPMailer(true);
 
 //Authentification
 $mail->isSMTP();
 $mail->SMTPAuth = true;
 //Connexion au serveur
-$mail->Host = 'hoziodev.fr';
-$mail->Port = 993;
-$mail->Username = 'mnrt.cinema@hoziodev.fr';
-$mail->Password = 'k_VHpwAv4PjC';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+//$mail->Username = 'project.hsp440@gmail.com';
+//$mail->Password = 'owtansfzyivxtnmb';
+$mail->Username = 'nicolascharpentiertest@gmail.com';
+$mail->Password = 'kxgbzlhrdlfzcgnp';
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 $mail->charset = 'UTF-8';
 
 //Envoie de mail
-$mail->setFrom('nicolascharpentiertest@gmail.com');
+$mail->setFrom('project.hsp440@gmail.com');
 $mail->addAddress($_POST['email']);
 $mail->Subject = 'MNRT Cinema Réinitialisation de votre mot de passe';
 $mail->Body = 'ICI SE TROUVE UN LIEN DE <b>REINITIALISATION</b> DE VOTRE MDP';
-$mail->send();
+try {
+    $mail->send();
+    echo "Message has been sent";
+} catch (\PHPMailer\PHPMailer\Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 
-header('Location: ../../index.php');
+header('Location: ../../vue/index.php');
 
