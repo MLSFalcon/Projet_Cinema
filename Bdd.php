ebff8c1abdded8074517187964dff28cb9df418a
Mateo LAPETRE <m.lapetre-sarthou@lprs.fr>
2025-03-21 08:24:26 +0100

    fix: BDD

---
+++ Bdd.php
@@ -0,0 +1,16 @@
+<?php
+class Bdd
+{
+
+    private $bdd;
+
+    function __construct()
+    {
+        $this->bdd = new PDO('mysql:host=127.0.0.1.fr;dbname=mnrt_cinema;charset=utf8', 'root', 'toor');
+    }
+
+    function getBdd()
+    {
+        return $this->bdd;
+    }
+}
